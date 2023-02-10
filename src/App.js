2e450bca816358a76ff059450d27164abd945688
devshimul <sbshimul000@gmail.com>
2023-02-10 12:38:45 +0600

    Register From Validaiton Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from 'react-router-dom';
+import './App.css'; 
+import Register from './pages/Register'
+import Login from './pages/Login'
+let router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Register />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/config/Config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5nQcBjbw_MTBWqeTs_OKG-eAc6QmCBSU",
+  authDomain: "alap-42677.firebaseapp.com",
+  projectId: "alap-42677",
+  storageBucket: "alap-42677.appspot.com",
+  messagingSenderId: "172614996681",
+  appId: "1:172614996681:web:facf8e1b21987e9a93b318",
+  measurementId: "G-B12J3XZQJP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default firebaseConfig;
